16bf2dcf826568d05dd55098af866a40bdf88f6d
metamatong <metamaton00@gmail.com>
2022-04-07 17:04:52 +0900

    프로젝트 생성

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,22 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+/**
+ * 문서 용도의 주석은 슬래쉬와 아스테리스크 두 개를 파일의 최상단 위치에서 내부 내용을 설명하는 용도로 사용할 수 있다.
+ */
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+/*
+  여러 줄의 주석은 슬래쉬와 아스테리스크로 만들 수 있다.
+ */
+// 한 줄짜리 주석은 슬래쉬 두번이면 된다.
+
+
+
+fun main(args: Array<String>) {
+    println("안녕하세요, Shoppi에 오신 것을 환영합니다!")
+    println("쇼핑을 계속 하시려면 이름을 입력해줘잉")
+
+    val name = readLine()
+    println("""
+        감사합니다. 반갑습니다, $name 님
+        원하시는 카테고리를 입력해주세요.
+        ***=======================================***
+    """.trimIndent())
 }
