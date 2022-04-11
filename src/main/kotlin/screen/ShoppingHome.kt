78855477da806d9f5a150bfa1964d4fb8795a0b1
metamatong <metamaton00@gmail.com>
2022-04-11 18:27:43 +0900

    Home 화면과 Category 화면으로 분리

--- src/main/kotlin/screen/ShoppingHome.kt
+++ src/main/kotlin/screen/ShoppingHome.kt
@@ -2,44 +2,28 @@ package screen
 
 class ShoppingHome {
 
-        fun start() {
-                showWelcomeMessage()
-                showCategories()
-        }
+    fun start() {
+        showWelcomeMessage()
+        showCategories()
+    }
 
-        private fun showCategories() {
+    private fun showWelcomeMessage() {
+        println("안녕하세요, Shoppi에 오신 것을 환영합니다!")
+        println("쇼핑을 계속 하시려면 이름을 입력해줘잉")
 
-                val categories = arrayOf("패션", "전자기기", "반려동물용품")
-                for (category in categories) {
-                        println(category)
-                }
-                println("=> 장바구니로 이동하시려면 #을 입력해주세요")
-
-                var selectedCategory = readLine()
-                while (selectedCategory.isNullOrBlank()) {
-                        println("값을 입력해주세요")
-                        selectedCategory = readLine()
-                }
-                if (selectedCategory == "#") {
-                        // TODO 1. 장바구니로 이동하도록 설정
-                } else {
-                        // TODO 2. 카테고리 상품 목록 보여주기
-                        // TODO 3. 목록에 없는 값을 입력한 경우.
-                }
-        }
-
-        private fun showWelcomeMessage() {
-                println("안녕하세요, Shoppi에 오신 것을 환영합니다!")
-                println("쇼핑을 계속 하시려면 이름을 입력해줘잉")
-
-                val name = readLine()
-                println(
-                        """
+        val name = readLine()
+        println(
+            """
                         감사합니다. 반갑습니다, $name 님
                         원하시는 카테고리를 입력해주세요.
                         ***=======================================***
                      """.trimIndent()
-                )
-        }
+        )
 
+    }
+
+    private fun showCategories() {
+        val shoppingCategory = ShoppingCategory()
+        shoppingCategory.showCategories()
+    }
 }
